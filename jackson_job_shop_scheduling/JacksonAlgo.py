8c20bfaebfac0f33f9534cfc8e0f4e0f7484925b
th-rpy <thamer.saraei@usherbrooke.ca>
2022-05-28 22:26:45 -0400

    Save and create pdf file

--- jackson_job_shop_scheduling/JacksonAlgo.py
+++ jackson_job_shop_scheduling/JacksonAlgo.py
@@ -1,6 +1,6 @@
 """Example of code."""
 
-from utils import gant_list, create_dir
+from utils import gant_list, create_dir, func_trait
 import data 
 import os
 import shutil
@@ -8,23 +8,14 @@ import getpass
 import hashlib
 import sys
 import io
-import email.mime.application
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 import mimetypes
 import smtplib
 from random import randrange
 import numpy as np
+import matplotlib.pyplot as plt
 import csv
 import operator
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
-from reportlab.lib import colors as cl
-from reportlab.lib.enums import TA_JUSTIFY
-from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, Table, TableStyle
-from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.lib.units import inch
+
 
 class JackAlgo():
     
@@ -117,14 +108,22 @@ class JackAlgo():
     def gant_data(self):
 
         global list_data_copy, gant_data, list_list_gant
+        create_dir(self.output_dir)
         flatten_result = self.get_cmax_virtual()[0]
         list_data = []
         gant_data = []
         l_ = self.nb_jobs + 1
+        klk, klks = {}, {}
         list_list_gant = []
         for i in range(len(flatten_result) // l_):
             list_data.append(flatten_result[i * l_:(i + 1) * l_])
         for p in range(len(list_data)):
+            h, hc = [], []
+            for i in range(len(list_data[p]) - 1):
+                #print("{}".format(s[p][i]))
+                h.append(list_data[p][i])
+                fvg = str(list_data[p][i])+"=>"
+                hc.append(fvg)
             db1, db3 = [], []
             list_excel = []
             #print(list_data[p])
@@ -170,13 +169,93 @@ class JackAlgo():
                 list_excel.append(db4)
                 #print(list_excel)
             list_list_gant.append(list_excel)
-        return list_list_gant, list_data, gant_data, self.nb_jobs, self.nb_machines
+            
+            txt_gant = open("output/TxtsOutput/gantt_file({0}).txt".format(p), "w")
+            k = 0
+            for i in list_excel:
+                k += 1
+                ch = "{0},".format(k)
+                for j in i:
+                    ch += "{0},{1},".format(j[0], j[1])
+                ch1 = ch[:-1] + "\n"
+                txt_gant.write(ch1)
+            txt_gant.close()
+            f = int(l_ / 2) + 1
+            new = np.array(np.loadtxt("output/TxtsOutput/gantt_file({0}).txt".format(
+                p), delimiter=",", unpack=True))
+            #print(new)
+            htl = []
+            for j in range(1, new.shape[0]):
+                htl.append(new[j][0])
+            # print(htl)
+            ht = list(set(htl))
+            ht = sorted(ht)
+            htt1 = [(ht[i] + ht[i + 1]) / 2 for i in range(len(ht) - 1)]
+            # print(set(htl))
+            htt = sorted(htt1)
+            #print(htt)
+            cmap = plt.get_cmap("gnuplot")
+            colors = [cmap(i) for i in np.linspace(0, 1, 2 * l_)]
+            for i in range(1, new.shape[0] - 1, 2):
+                plt.hlines(new[0], new[i], new[i + 1], colors=colors[i], lw=28)
+                plt.text(htt[int((i - 1) / 2)], 1, str(h[int(i / 2)]))
+                cv = 0.0
+                while cv < new[0][0]:
+                    if cv == 0.0:
+                        plt.text(new[i][0]-0.1/2, cv-0.25,
+                                str(new[i][0]), color=colors[i])
+                        plt.text(new[i+1][0]-0.1/2, cv-0.25,
+                                str(new[i+1][0]), color=colors[i])
+                    plt.text(new[i][0]-0.1, cv, "|", color=colors[i])
+                    plt.text(new[i+1][0]-0.1, cv, "|", color=colors[i])
+                    cv += 0.2
+                plt.grid(True)
+                
+            for j in range(1, new.shape[1]):
+                for o in range(1, new.shape[0]):
+                    htl.append(new[o][0])
+                ht = list(set(htl))
+                htt = [(ht[i] + ht[i + 1]) / 2 for i in range(len(ht) - 1)]
+                for i in range(1, new.shape[0] - 1, 2):
+                    plt.hlines(new[0][j], new[i][j], new[i + 1]
+                            [j], colors=colors[i], lw=28, )
+                    plt.text((new[i + 1][j] + new[i][j]) / 2,
+                            new[0][j], str(h[int(i / 2)]))
+                    cv = 0.0
+                    while cv < new[0][j]:
+                        if cv == 0.0:
+                            plt.text(new[i][j]-0.05, cv+(new[0][j]-1)
+                                    * .1, str(new[i][j]), color=colors[i])
+                            plt.text(new[i+1][j]-.05, cv+(new[0][j]-1)
+                                    * .1, str(new[i+1][j]), color=colors[i])
+                        plt.text(new[i][j]-0.1, cv, "|", color=colors[i])
+                        plt.text(new[i+1][j]-0.1, cv, "|", color=colors[i])
+                        cv += 0.2
+                    plt.grid(True)
+            
+            plt.xlabel("time(j)")
+            plt.title("Ordanncement FlowShop: {0} machines et {1} Taches".format(b + 1,
+                                                                             l_ - 1) + "\n" + "\t" + "Diagramme de Gantt (seq ={0} avec Cmax({2})={1})".format(
+            h, list_excel[-1][-1][-1], p))
+            chh = ""
+            for i in hc:
+                chh += i
+            chh = chh[:-2]
+            klk[chh] = [list_data[p][l_-1], list_excel[-1][-1][-1]]
+            klks[chh] = [list_excel[-1][-1][-1]]
+            plt.ylim(0, b + 2)
+            plt.xlim([0, list_excel[-1][-1][-1] + 2])
+            plt.margins(0, 1)
+            S = func_trait(list_excel[-1][-1][-1], b+1, 0, "|")
+            plt.annotate("Cmax=" + str(list_excel[-1][-1][-1]), xy=(list_excel[-1][-1][-1], b+1), xytext=(
+                0.75*list_excel[-1][-1][-1], b+1.6), arrowprops=dict(facecolor='green', shrink=0.05))
+            plt.text( list_excel[-1][-1][-1], b + 1, S )
+            manager = plt.get_current_fig_manager()
+            plt.savefig("output/ImagesOutput/output_diagram_gantt({0}).png".format(p), bbox_inches='tight')
+            
+        return colors, list_list_gant, list_data, gant_data, self.nb_jobs, self.nb_machines
     
-    def save_output(self):
-        pdf_path = create_dir(self.path_output)
-        return pdf_path
-    
-    def plot_gant(self):
+        '''def plot_gant(self):
 
         global list_data_copy, gant_data, list_list_gant
         flatten_result = self.get_cmax_virtual()[0]
@@ -185,7 +264,7 @@ class JackAlgo():
         for i in range(len(flatten_result) // l_):
             list_data.append(flatten_result[i * l_:(i + 1) * l_])
         for p in range(len(list_data)):
-            ded = open("output/TxtsOutput/gantt__file({0}).txt".format(p), "w")
+            ded = open("output/TxtsOutput/gantt_file({0}).txt".format(p), "w")
             k = 0
 
             for i in self.gant_data()[0]:
@@ -199,10 +278,10 @@ class JackAlgo():
             ded.close()
             f = int(l_ / 2) + 1
             
-        return ch1
+        return ch1'''
             
     def __str__(self):
-        return str(self.plot_gant())
+        return str(self.gant_data()[0])
     
 
 data_path = 'jackson_job_shop_scheduling/jackson_job_shop_scheduling/input.txt'       
--- jackson_job_shop_scheduling/utils.py
+++ jackson_job_shop_scheduling/utils.py
@@ -1,7 +1,24 @@
-
 import os
-import sys
 import shutil
+import getpass
+import hashlib
+import sys
+import io
+import mimetypes
+import smtplib
+from random import randrange
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import operator
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from reportlab.lib import colors as cl
+from reportlab.lib.enums import TA_JUSTIFY
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, Table, TableStyle
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import inch
 
 def gant_list(C):
     db1, db2 = [], []
@@ -23,9 +40,47 @@ def create_dir(path):
         shutil.rmtree(dir)
     os.makedirs(dir)
 
-    os.makedirs('output/ImagesOutput')
-    os.makedirs('output/PdfsOutput')
-    os.makedirs('output/TxtsOutput')
-    pdf_name = "output/PdfsOutput/Algo_Cds_Output.pdf"
+    os.makedirs(dir + '/ImagesOutput')
+    os.makedirs(dir + '/PdfsOutput')
+    os.makedirs(dir + '/TxtsOutput')
+    pdf_name = dir + "/PdfsOutput/Algo_Cds_Output.pdf"
     
-    return pdf_name
+def func_trait(x, y, h, s):
+    S = ""
+    j = y
+    while j > h:
+        j -= 0.5
+        S += x * " " + s + "\n"
+    return S
+
+def create_pdf_file(story, pdf_name = 'Algo_Cds_Output.pdf'): 
+
+    doc = SimpleDocTemplate(pdf_name, pagesize=letter,
+                            rightMargin=15, leftMargin=15,
+                            topMargin=15, bottomMargin=20)
+    
+    Story = []
+    tit = "Job Shop Scheduling : N Tasks through M Machines"
+    stit = "1.  Background"
+    contexte = " A job shop consists of a set of distinct machines that process jobs. \
+    Each job is a series of tasks that require use of particular machines for known durations, and which must be completed in specified order. \
+    The job shop scheduling problem is to schedule the jobs on the machines to minimize the time necessary to process all jobs (i.e, the makespan) or some other metric of productivity. \
+    Job shop scheduling is one of the classic problems in Operations Research. "
+    contexte_2 = "Data consists of two tables. The first table is decomposition of the jobs into a series of tasks. \
+    ach task lists a job name, name of the required machine, and task duration. \
+    The second table list task pairs where the first task must be completed before the second task can be started. \
+    This formulation is quite general, but can also specify situations with no feasible solutions."
+    styles = getSampleStyleSheet()
+    styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
+    ptext = '<font size=20 color=red>%s</font>' % tit
+    stit = '<font size=15 color=black>%s</font>' % stit
+    Story.append(Paragraph(ptext, styles["BodyText"]))
+    Story.append(Spacer(5, 30))
+    Story.append(Paragraph(stit, styles["BodyText"]))
+    Story.append(Spacer(5, 15))
+    Story.append(Paragraph(contexte, styles["BodyText"]))
+    Story.append(Spacer(5, 10))
+    Story.append(Paragraph(contexte_2, styles["BodyText"]))
+    doc.build(Story)
+    
+create_pdf_file('story','Algo_Cds_Output.pdf')
